037fa75ceb5f0039b123b2364eb36a08ff35140e
sou412 <sou412snow@gmail.com>
2019-09-23 22:31:49 -0400

    update data

---
+++ R/orddid-data.R
@@ -0,0 +1,7 @@
+#' Dataset from Barney and Schaffner (2018)
+#' 
+#' @name barney_schaffner_18
+#' @docType data 
+#' @references Barney and Schaffner (2018)
+#' @keywords dataset 
+"barney_schaffner_18"
