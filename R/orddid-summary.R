fa3b7a99c0ce57025fece03cdfdd9ecb3f69c190
soichiro <sou412snow@gmail.com>
2020-02-08 22:34:24 -0500

    del link [skip travis]

--- R/orddid-summary.R
+++ R/orddid-summary.R
@@ -1,6 +1,6 @@
 
 #' Summary function
-#' @param obj an output object from \link{\code{orddid}} or \link{\code{equivalence_test}}.
+#' @param obj an output object from \code{orddid} or \code{equivalence_test}.
 #' @param cumulative Default is \code{TRUE}. Only effective for displaying the causal effects.
 #'  If \code{TRUE}, cumulative effect is presented.
 #' @export
