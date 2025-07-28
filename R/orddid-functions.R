6106d95ba91ddde96e42e1d988d779379b9cb9ed
sou412 <sou412snow@gmail.com>
2025-07-28 14:47:35 -0400

    Remove deprecated functions

--- R/orddid-functions.R
+++ R/orddid-functions.R
@@ -1,197 +1,90 @@
-
-#' Estimate the baseline parameters using Y00 (t = 0, d = 0) observations
+#' Estimate parameters for the observed outcome
 #' 
+#' @noRd
+#' @keywords internal
+.EstimateControlParams <- function(Y00, Y01, Y10) {
+  # This ordering of estimation assumes that we fix sd_00 = 1.
+  # Y00: Estimate mu_00, and the cutoffs
+  fit_y00 <- .EstimateLatentOutcomeParams(Ydt = Y00, cutoff = NULL)
+
+  # Y01: Estimate mu_01 and sd_01 with cutoff fixed
+  fit_y01 <- .EstimateLatentOutcomeParams(Ydt = Y01, cutoff = fit_y00$cutoff)
+
+  # Y10: Estimate mu_10 and sd_10 with cutoff fixed
+  fit_y10 <- .EstimateLatentOutcomeParams(Ydt = Y10, cutoff = fit_y00$cutoff)
+
+  return(list(y00 = fit_y00, y01 = fit_y01, y10 = fit_y10))
+}
+
+#' Estimate parameters for the latent outcome (mu_dt, sd_dt)
+#'
 #' @param Y00 A vector of pre-treatment control observations.
 #' @param cutoff A value for the first cutoff value to be fixed. Default is 0.
-#'  
+#'
 #' @noRd
-.EstimateBaselineParams <- function(Y00, cutoff = 0) {
-
+.EstimateLatentOutcomeParams <- function(Ydt, cutoff) {
   ## Implement the ordinal probit regression only with the intercept.
-  fit <- .FitOrderedProbit(Y = Y00, init = NULL, cut = cutoff)
+  fit <- .FitOrderedProbit(Y = Ydt, cutoff_val = cutoff)
   return(list(mu = fit$mu, sd = fit$sd, cutoff = fit$cutoff))
 }
 
-#' Parameter Estimation for the Counterfactual Outcome
+#' Estimate parameters for the counterfactual outcome.
 #'
-#' @keywords internal
-ord_did_run <- function(Ynew, Yold, treat, cut, pre = FALSE) {
-  ## input check
-  ord_did_run_check_input(Ynew, Yold, treat)
-  dat <- create_group_dummy(Ynew, Yold, treat, pre = pre)
+#' Use the identification formula to estimate mu_11 and sd_11
+#' @noRd
+.EstimateCounterfactualParams <- function(fit) {
+  mu11 <- with(fit, y10$mu + (y01$mu - y00$mu) * (y10$sd / y00$sd))
+  sd11 <- with(fit, y10$sd * y01$sd / y00$sd)
 
-  ## fit probit regression
-  fit_ct <- fit_ord_probit_gr(Y = dat$Y, id_group = dat$id_group, cut = cut)
-  fit_tr <- fit_ord_probit(Y = Ynew[treat == 1], cut = cut)
+  return(list(mu = mu11, sd = sd11, cutoff = fit$y00$cutoff))
+}
 
-  ## estimated params
-  mu00 <- fit_ct$mu[1]; mu01 <- fit_ct$mu[2]; mu10 <- fit_ct$mu[3]
-  sd00 <- fit_ct$sd[1]; sd01 <- fit_ct$sd[2]; sd10 <- fit_ct$sd[3]
+.EstimateOutcomeProportions <- function(mu, sd, cutoff) {
+  # calculate the cutoff values
+  cut_new <- .PadInf(cutoff)
+  prob_yj <- rep(NA, (length(cutoff) + 1))
 
-  if (isTRUE(pre)) {
-    ## fit_ct incldus "post-treatment x treatment" if pre = TRUE
-    mu11 <- fit_ct$mu[4]
-    ss11 <- fit_ct$sd[4]
-  } else {
-    ## identify mean and variance of Y11
-    mu11 <- mu10 + (mu01 - mu00) * (sd10 / sd00)
-    ss11 <- sd10 * sd01 / sd00
+  for (j in 1:(length(cutoff) + 1)) {
+    prob_yj[j] <- pnorm(cut_new[j + 1], mean = mu, sd = sd) -
+      pnorm(cut_new[j], mean = mu, sd = sd)
   }
 
-  ##
-  ## compute the probability Pr(Y(0) = j | D = 1)
-  ##
-  cutoff <- fit_ct$cutoff
-  cut_new <- c(-Inf, cutoff, Inf)
-  Ypred <- rep(NA, (length(cutoff)+1))
-  for (j in 1:(length(cutoff)+1)) {
-    Ypred[j] <- pnorm(cut_new[j+1], mean = mu11, sd = ss11)  -
-                pnorm(cut_new[j], mean = mu11, sd = ss11)
-  }
+  return(prob_yj)
+}
 
-  ##
-  ## compute observed Pr(Y(1) = j | D = 1)
-  ##
-  Yobs <- as.vector(prop.table(table(Ynew[treat==1])))
+.PadInf <- function(x) {
+  return(c(-Inf, x, Inf))
+}
 
-  ##
-  ## estimatd parameters
-  ##
-  theta_est <- list(
-    mu00 = mu00, sd00 = sd00,
-    mu01 = mu01, sd01 = sd01,
-    mu10 = mu10, sd10 = sd10,
-    mu11 = mu11, sd11 = ss11
-  )
 
-  return(list("Y1" = Yobs, "Y0" = Ypred, 'mu11' = mu11, 'ss11' = ss11,
-    fit_ct = fit_ct, fit_tr = fit_tr, theta = theta_est
+
+.ComputeTreatmentEffects <- function(y1_prop, y0_prop) {
+  effect_cumulative <- .CumulativeEffects(y1_prop, y0_prop)
+  return(list(
+    effect_cumulative = effect_cumulative,
+    y1_prop = y1_prop,
+    y0_prop = y0_prop
   ))
 }
 
+.CumulativeEffects <- function(y1_prop, y0_prop) {
+  effect_est <- map2(y1_prop, y0_prop, function(y1, y0) {
+    # Calculate cumulative effects
+    cum_y1 <- cumsum(y1)
+    cum_y0 <- cumsum(y0)
 
-#' Bootstrap function
-#'
-#' Conduct a block bootstrap at the unit level to compute variances and CIs
-#' @keywords internal
-#' @param Ynew A vector of post-treatment observations.
-#' @param Yold A vector of pre-treatment observations.
-#' @param treat A vector of treatment indicator (should be either 0 or 1).
-#' @param id_cluster A vector of clustering index. This should be the same length as \code{treat} vector.
-#' @param J The number of categories.
-#' @param n_boot Number of bootstrap iterations.
-#' @param verbose A boolean argument.
-ord_did_boot <- function(Ynew, Yold, treat, cut, id_cluster, J, n_boot, verbose, pre) {
+    # Calculate treatment effects
+    return(cum_y1 - cum_y0)
+  })
 
-  ## create an object to save bootparams
-  boot_save <- list()
-  boot_save[[1]] <- boot_save[[2]] <- matrix(NA, nrow = n_boot, ncol = J)
-  boot_save[[3]] <- boot_save[[4]] <- rep(NA, n_boot)
-  boot_params_save <- list()
+  # Compute the quantile based CI
+  effect_mat <- bind_rows(effect_est)
+  effect_ci <- apply(effect_mat, 2, function(x) {
+    quantile(x, probs = c(0.025, 0.975))
+  })
 
-  ## check verbose
-  if (isTRUE(verbose)) {
-    iter_show <- round(n_boot * 0.1)
-  }
-
-  ## convert id_cluster to numeric
-  if (!is.null(id_cluster)) {
-    id_cluster <- as.numeric(as.factor(id_cluster))
-  }
-
-
-  ##
-  ## setup for bootstrap
-  ##
-  # define an iterator
-  b <- 1
-  # prep data
-  dat_tmp <- cbind(Ynew, Yold, treat)
-  if (!is.null(id_cluster)) {
-    id_unique <- unique(id_cluster)   # unique cluster id
-    n_cluster <- length(id_unique)
-    max_cluster_size <- max(table(id_cluster))
-  }
-
-  ## bootstrap -----------------------------------------------------------
-  # reject the bootstrap replica when optimization fails
-  # typically rejection happens when outcome is not well balanced
-  while(b <= n_boot) {
-    tryCatch({
-      # sample bootstrap index
-      if (is.null(id_cluster)) {
-        dat_boot <- block_unit_sample(dat_tmp)
-      } else {
-        dat_boot <- block_sample(dat_tmp, id_cluster, id_unique,
-          n_cluster, max_cluster_size)
-      }
-
-
-      # fit the model
-      fit_tmp <- ord_did_run(
-        Ynew  = dat_boot[, 1],
-        Yold  = dat_boot[ ,2],
-        treat = dat_boot[, 3],
-        cut   = cut,
-        pre   = pre 
-      )
-
-      # save estimates
-      boot_save[[1]][b,]    <- fit_tmp$Y1  # Yobs
-      boot_save[[2]][b,]    <- fit_tmp$Y0  # Y(0)
-      boot_save[[3]][b]     <- fit_tmp$mu11
-      boot_save[[4]][b]     <- fit_tmp$ss11
-      boot_params_save[[b]] <- unlist(fit_tmp$theta)
-
-      # update iterator
-      b <- b + 1
-
-    }, error = function(e) {
-      NULL
-    })
-
-    ## verbose ----------------------------------------------------
-    if (isTRUE(verbose)) {
-      if ((b %% iter_show) == 0) {
-          cat('\r', b, "out of", n_boot, "bootstrap iterations")
-          flush.console()
-      }
-    }
-    ## end of verbose ---------------------------------------------
-
-  }
-  ## end of bootstrap iterations -----------------------------------------
-
-  # clear the console
-  cat("\n")
-
-  # concatenate all params
-  boot_params <- do.call("rbind", boot_params_save)
-
-  return(list("boot_params" = boot_params, "boot_save" = boot_save))
+  return(effect_ci)
 }
 
+.RelativeEffect <- function() {}
 
-
-#' Block re-sampling for block bootstrap
-#'
-#' @param dat a data frame with \code{Ynew}, \code{Yold} and \code{treat}.
-#' @param id_cluster a cluster id vector of length n.
-#' @return a list of resampled data.
-#' @keywords internal
-block_sample <- function(dat, id_cluster, id_unique, n_cluster, max_cluster_size) {
-  # sample cluster id & data
-  id_cluster_boot <- sample(id_unique, size = n_cluster, replace = TRUE)
-  dat_boot <- dat_block_boot(dat = dat, id_cluster = id_cluster,
-    id_cluster_boot = id_cluster_boot, max_cluster_size = max_cluster_size)
-  return(dat_boot)
-}
-
-
-#' Unit level block bootstrap
-#' @keywords internal
-block_unit_sample <- function(dat) {
-  idx_use  <- sample(1:nrow(dat), size = nrow(dat), replace = TRUE)
-  dat_boot <- dat[idx_use, ]
-  return(dat_boot)
-}
